78b0192072383f2968e2c584e6031d8688e007df
Harini Kondisetty <harini.kondisetty@gmail.com>
2025-07-11 10:31:47 -0500

    We created a birthday app

---
+++ Birthdays/Friend.swift
@@ -0,0 +1,20 @@
+//
+//  Friend.swift
+//  Birthdays
+//
+//  Created by Scholar on 7/11/25.
+//
+
+import Foundation
+
+class Friend
+{
+    var name = ""
+    var birthday = Date()
+    
+    init(name: String, birthday: Date)
+    {
+        self.name = name
+        self.birthday = birthday
+    }
+}
